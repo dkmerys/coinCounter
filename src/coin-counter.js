982f8fba4f5b6bf9f996118a4004820848e85b14
Daniel Merys <dkmerys@gmail.com>
2020-06-22 15:35:20 -0700

    remove unnecessary code

--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,17 +1,15 @@
+const recursiveCounter = (totalAmountInPennies, coinChangeArr = [], counter = 0) => {
+  const coins = [25, 10, 5, 1];
+  if (counter >= 4) {
+    return `It would take ${coinChangeArr[0]} quarters, ${coinChangeArr[1]} dimes, ${coinChangeArr[2]} nickles, and ${coinChangeArr[3]} pennies to form your change.`
 
-
-// const recursiveCounter = (totalAmountInPennies, coinChangeArr = [], counter = 0) => {
-//   const coins = [25, 10, 5, 1];
-//   if (counter >= 4) {
-//     return `It would take ${coinChangeArr[0]} quarters, ${coinChangeArr[1]} dimes, ${coinChangeArr[2]} nickles, and ${coinChangeArr[3]} pennies to form your change.`
-
-//   } else {
-//     const amount = Math.floor(totalAmountInPennies / coins[counter]);
-//     const remainder = totalAmountInPennies - (amount * coins[counter]);
-//     const newCoinArr = [...coinChangeArr, amount];
-//     return recursiveCounter(remainder, newCoinArr, counter + 1);
-//   }
-// }
+  } else {
+    const amount = Math.floor(totalAmountInPennies / coins[counter]);
+    const remainder = totalAmountInPennies - (amount * coins[counter]);
+    const newCoinArr = [...coinChangeArr, amount];
+    return recursiveCounter(remainder, newCoinArr, counter + 1);
+  }
+}
 
 const curriedCounter = (amount) => {
   return (quarter) => {
@@ -29,79 +27,9 @@ const curriedCounter = (amount) => {
     }
   }
 }
-// const curriedCounter = (amount) => {
-//   // if (amount >= .25) {
-//   const altAmount = amount * 100
-//   const quarters = Math.floor(altAmount / 25)
-//   const dimesRemainder = altAmount % 25
-//   console.log(dimesRemainder)
-//   return (dimesRemainder) => {
 
-//     const dimes = Math.floor(dimesRemainder / 10)
-//     console.log(dimesRemainder)
-//     console.log(dimes)
-//     const nicklesRemainder = dimesRemainder % 10
-//     return (nicklesRemainder) => {
-//       const nickles = Math.floor(nicklesRemainder / 5)
-//       const pennies = nicklesRemainder % 5
-//       finalString = `It will take ${quarters} quarters, ${dimes} dimes, ${nickles} nickles, and ${pennies} pennies to turn ${amount} into change.`
-//       return finalString
-//     }
-//   }
-// }
-// function volume(l) {
-//   return (w) => {
-//     return (h) => {
-//       return l * w * h
-//     }
-//   }
-// }
-
-// if (amount >= .10) {
-//   dimes = Math.floor(amount / .10)
-//   return coinCounter(amount - (dimes * .10))
-//   // console.log(amount - (dimes * .10))
-// } else {
-//   if (amount >= .05 && !dimes) {
-//     dimes = 0
-//     nickles = Math.floor(amount / .05)
-//     return coinCounter(amount - (nickles * .05))
-//   } else if (amount >= .05 && dimes) {
-//     nickles = Math.floor(amount / .05)
-//     return coinCounter(amount - (nickles * .05))
-//   } else {
-//     if (amount < .05 && !dimes && !nickles) {
-//       dimes = 0
-//       nickles = 0
-//       pennies = Math.floor(amount / .01)
-//       return `${ quarters } quarters, ${ dimes } dimes, ${ nickles } nickles, and ${ pennies } pennies are needed to convert your amount to coins.`
-//     }
-//     else if (amount < .05 && !dimes) {
-//       dimes = 0
-//       pennies = Math.floor(amount / .01)
-//       return `${ quarters } quarters, ${ dimes } dimes, ${ nickles } nickles, and ${ pennies } pennies are needed to convert your amount to coins.`
-//     }
-//     else if (amount < .05 && !nickles) {
-//       nickles = 0
-//       pennies = Math.floor(amount / .01)
-//       return `${ quarters } quarters, ${ dimes } dimes, ${ nickles } nickles, and ${ pennies } pennies are needed to convert your amount to coins.`
-//     }
-//     else {
-//       pennies = Math.floor(amount / .01)
-//       return `${ quarters } quarters, ${ dimes } dimes, ${ nickles } nickles, and ${ pennies } pennies are needed to convert your amount to coins.`
-//     }
-//   }
-// }
-// }
-// }
-
-// const coinCounter = (amount) => {
-//   if (amount < .05 && amount > 0) {
-//     pennies = Math.floor(amount / .01)
-//     return
-//   }
-// }
 
 const total = curriedCounter(5.83)(25)(10)(5)
-
+const total2 = recursiveCounter(583)
 console.log(total)
+console.log(total2)
